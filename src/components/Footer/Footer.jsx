ceab6a894640b40ebf26a8c04c550dbfb2d5dcce
Levon Avetisyan <avetisyan.levon@gmail.com>
2019-07-18 11:45:07 +0300

    webpack configuration - done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.scss'
+import React, {PureComponent} from 'react';
+
+export default class Footer extends PureComponent {
+    static defaultProps = {}
+    
+    render() {
+        return (
+            <div className="Footer"></div>
+        )
+    }
+}
