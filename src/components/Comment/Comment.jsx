35a70ddd02f9f7f7b1b58771ccd8edcf991ba450
Levon Avetisyan <avetisyan.levon@gmail.com>
2019-07-30 16:22:15 +0300

    test app

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,12 @@
+import './Comment.scss'
+import React, {PureComponent} from 'react';
+
+export default class Comment extends PureComponent {
+    static defaultProps = {}
+    
+    render() {
+        return (
+            <div className="Comment"></div>
+        )
+    }
+}
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,12 @@
+import './Form.scss'
+import React, {PureComponent} from 'react';
+
+export default class Form extends PureComponent {
+    static defaultProps = {}
+    
+    render() {
+        return (
+            <div className="Form"></div>
+        )
+    }
+}
